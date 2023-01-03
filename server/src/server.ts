6cebcbc31f1df46519ef585411b3028f0a4dfa7a
uityh <matthew6600@gmail.com>
2023-01-03 15:56:43 -0500

    init

--- server/src/server.ts
+++
@@ -1,6 +0,0 @@
-import app from './app';
-const port = 4000;
-
-app.listen(port, () => {
-    console.log(`Server is listening on port ${port} : https://localhost:${port}`);
-    });
---
+++ src/server.ts
@@ -0,0 +1,8 @@
+import app from "./app";
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+  console.log(
+    `Server is listening on port ${port} : https://localhost:${port}`
+  );
+});
